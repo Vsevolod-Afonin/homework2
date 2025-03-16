c3140ac15c0b39de068abb004a1e0e0593530293
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-03-16 17:28:28 +0700

    Full programm

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 class User():
-    def __init__(self, ID, name, access_level):
-        self.ID = ID
-        self.name = name
-        self.access_level = access_level
+    def __init__(self, ID, name):
+        self.__ID = ID
+        self.__name = name
+        self.__access_level = 'user'
 
-    def info(self):
-        print(f'ID - {self.ID}')
-        print(f'Имя - {self.name}')
-        print(f'Уровень доступа - {self.access_level}')
+    def get_ID(self):
+        return self.__ID
+
+    def get_name(self):
+        return self.__name
+
+    def get_access_level(self, access_level):
+        return self.__access_level
+
+    def set_name(self, name):
+        self.__name = name
+
+
+class Admin(User):
+    def __init__(self, ID, name):
+        super().__init__(ID, name)
+        self.__access_level = 'admin'
+
+    def add_user(self, userlist, user):
+        userlist.append(user)
+        print('Пользователь успешно добавлен в список')
+        print(userlist)
+
+    def remove_user(self, userlist, user):
+        userlist.remove(user)
+        print('Пользователь был успешно удалён')
+        print(userlist)
+
+users = []
+admin = Admin('777', 'Вася')
+user1 = User('6473246872', 'Петя')
+
+print(user1.get_name())
+admin.add_user(users, user1)
+print(users)
